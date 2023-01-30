1b4ab35bfa6e0decb28d120162f513072fad45c4
Mezan2002 <mezanurr009@gmail.com>
2023-01-30 14:12:33 +0600

    Almost done

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ app.use(express.json());
 
 // initial setup of express start
 
+// jwt token start
+const verifyJWT = (req, res, next) => {
+  console.log(req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "Unauthorized Access!" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      res.status(401).send({ message: "Unauthorized Access!" });
+    }
+    req.decoded = decoded;
+  });
+  next();
+};
+// jwt token end
+
 // connect the mongodb start
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
@@ -63,17 +81,41 @@ const run = async () => {
     // add billing API end
 
     // get billing list API start
-    app.get("/billing-list", async (req, res) => {
+    app.get("/billing-list", verifyJWT, async (req, res) => {
+      const activePage = parseInt(req.query.activePage);
+      const dataPerPage = parseInt(req.query.dataPerPage);
       const query = {};
-      const result = await billings.find(query).toArray();
-      res.send(result);
+      const options = {
+        sort: { createdTime: -1 },
+      };
+      const count = await billings.estimatedDocumentCount();
+      const result = await billings
+        .find(query, options)
+        .skip(activePage * dataPerPage)
+        .limit(dataPerPage)
+        .toArray();
+      res.send({ result, count });
     });
     // get billing list API end
 
     // update billing details API start
     app.patch("/update-billing/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      const filter = { _id: ObjectId(id) };
+      const fullName = req.body.fullName;
+      const email = req.body.email;
+      const phone = req.body.phone;
+      const payableAmount = req.body.payableAmount;
+      const updatedDoc = {
+        $set: {
+          fullName: fullName,
+          email: email,
+          phone: phone,
+          payableAmount: payableAmount,
+        },
+      };
+      const result = await billings.updateOne(filter, updatedDoc);
+      res.send(result);
     });
     // update billing details API end
 
@@ -85,6 +127,14 @@ const run = async () => {
       res.send(result);
     });
     // delete billing details API end
+
+    // create JWT API start
+    app.post("/jwt", async (req, res) => {
+      const user = req.query.email;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN);
+      res.send({ token: token });
+    });
+    // create JWT API end
   } finally {
   }
 };
